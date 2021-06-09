c34a25c7164c61a539fdd17b5a42ba1d8c6bbf72
Boris Parusnikov <2669997@gmail.com>
2021-06-09 15:26:11 +0300

    bootstrapped

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,21 +4,21 @@ class IndexController < ApplicationController
   def index
     @directory = Dir.entries(ROOT_DIR)
     @path = ''
-    @root_path = ROOT_DIR
+    @current_path = ROOT_DIR
   end
 
   def path
     check_path
 
-    if File.directory?(@root_path)
-      @directory = Dir.entries(@root_path)
+    if File.directory?(@current_path)
+      @directory = Dir.entries(@current_path)
       @path = "#{params[:path]}/"
       render :index
-    elsif File.file?(@root_path)
-      if File.size(@root_path) > 250_000
-        send_file @root_path
+    elsif File.file?(@current_path)
+      if File.size(@current_path) > 250_000
+        send_file @current_path
       else
-        @file = File.read(@root_path)
+        @file = File.read(@current_path)
         render :file
       end
     end
@@ -28,10 +28,10 @@ class IndexController < ApplicationController
 
   def check_path
     current_directory = File.expand_path(ROOT_DIR)
-    @root_path = File.expand_path(params[:path], ROOT_DIR)
-    raise ActionController::RoutingError, 'Not Found' unless File.exists?(@root_path)
+    @current_path = File.expand_path(params[:path], ROOT_DIR)
+    raise ActionController::RoutingError, 'Not Found' unless File.exists?(@current_path)
 
-    unless @root_path.starts_with?(current_directory)
+    unless @current_path.starts_with?(current_directory)
       raise ArgumentError, 'Should not be parent of root'
     end
   end
